10f6cdcc29b9a4caaf378ae54e13be00c2ee2dc6
Nedpereira <neder_pereira@hotmail.com>
2021-09-15 17:54:01 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+function clicou(){
+
+    document.getElementById("agradecimento"). innerHTML = "Obrigado por clicar";
+    //alert("Obrigado por clicar!");
+}
+
+function  redirecionar(){
+    //window.open("https://www.google.com.br/");
+    window.location.href = "https://www.google.com.br/";
+}
+
+
+/*
+function soma(n1, n2)
+{
+    return n1+n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if (idade>= 18)
+    {
+      validar = true;
+    }else{
+        validar= false
+    }
+   
+}
+
+alert(soma(5,10));
+
+*/
+/*
+var d = new Date();
+
+alert(d.getDay);
+alert(d.getHours);
+*/
+
+/*
+var count;
+for (count=0; count <=5; count++)
+{
+alert(count);
+}
+*/
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count = count +1;
+}
+*/
+/*
+var idade = prompt("Qual sua idade");
+if (idade > 18){
+    alert("maior de idade " + idade);
+}else {
+    alert("menor de idade");
+}
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+*/
+
+/*
+var lista =["maça", "pêra", "laranja"];
+lista.push("uva"); // Adioconar a lista 
+lista.pop(); //Remover o ultimo da lista
+console.log(lista[1])
+
+console.log(lista.reserve());
+console.log(lista.join(" - "));
+*/
+
+/*
+var nome = "Rafael Galleani";
+var n1 = 2;
+var n2 = 4;
+var frase = "Japão o melhor time do mundo";
+//alert(+ nome " tem " + idade " anos ");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toUpperCase()); 
+*/
+
